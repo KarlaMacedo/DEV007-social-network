607e29f0a430e9ce8c26313e31625ed81d8c2727
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-06-19 15:53:27 -0600

    Deploy exitoso

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-
 // additional config to direct the vite build to src directory
 // https://vitejs.dev/config/#conditional-config
 // https://vitejs.dev/config/shared-options.html#root
-
 export default defineConfig(({ command }) => {
   if (command === 'build') {
     return {
       root: 'src',
-      // para hacer un deploy en github pages, configura propiedad base con el
-      // nombre/url de tu repo
-      // para fazer uma implantação github pages, defina a propriedade base
-      // para o nome/url do seu repositório
-      // https://vitejs.dev/guide/static-deploy.html#github-pages
-      // ejemplo base: '/social-network/',
-      base: '/DEV007-social-network/',
       build: {
-        minify: 'esbuild',
-        target: 'esnext',
+        minify: false,
         rollupOptions: {
           output: {
             dir: './dist',
